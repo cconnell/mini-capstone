e0d00acb209e4de0d14e2255f9d5b7ae2f7b3737
Chris Connell <chris.g.connell@gmail.com>
2016-01-20 21:21:32 -0600

    end of class 1-20-16

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ActiveRecord::Base
+
+
+  def sale_notice
+    cost = price.to_i
+    if cost < 2
+      "Discount Item!"
+    else
+      "On Sale!"
+    end
+  end
+
+  def tax
+    tax = (price.to_f * 0.09).round(2)
+  end
+
+  # def tax
+  #   price * 0.09
+  # end
+
+  def total
+    total = (price.to_f * 0.09).round(2) + price.to_f 
+  end
+
+  # def total
+  #   price + tax
+  # end
+  
 end
